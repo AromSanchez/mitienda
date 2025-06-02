7fffb30f251da336ef5876de2a08f9d42db8c0ed
Arom Sanchez <160637122+AromSanchez@users.noreply.github.com>
2025-06-01 23:35:56 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: 'https://mitienda-production.up.railway.app/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
